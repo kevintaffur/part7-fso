e0f670983d86d845f0c3ab44790dbbc8f7df39eb
Kevin <86487929+tar83@users.noreply.github.com>
2022-11-01 11:30:32 -0500

    exercises 7.5 and 7.6

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -90,11 +90,17 @@ const CreateNew = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content: content.value,
-      author: author.value,
-      info: info.value,
+      content: content.field.value,
+      author: author.field.value,
+      info: info.field.value,
       votes: 0
     })
+  }
+
+  const handleReset = () => {
+    content.reset();
+    author.reset();
+    info.reset();
   }
 
   return (
@@ -103,17 +109,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input {...content} />
+          <input {...content.field} />
         </div>
         <div>
           author
-          <input {...author} />
+          <input {...author.field} />
         </div>
         <div>
           url for more info
-          <input {...info} />
+          <input {...info.field} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button type="reset" onClick={handleReset}>reset</button>
       </form>
     </div>
   )
